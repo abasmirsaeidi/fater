9e4fed84b53fc00cf555c7b664d39bb5c11c1071
Abbas Mirsaidi <usra1374@yahoo.com>
2018-07-02 10:31:37 +0430

    a littli change

--- src/app/information/information.module.ts
+++ src/app/information/information.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { InformationComponent } from './Information.component';
+import { InformationComponent } from './information.component';
 import {RouterModule, Routes} from '@angular/router';
 import {MDBBootstrapModule} from '../../../angular-bootstrap-md';
 
--- src/app/news/news.module.ts
+++ src/app/news/news.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NewsComponent } from './News.component';
+import { NewsComponent } from './news.component';
 import {RouterModule, Routes} from '@angular/router';
 import {MDBBootstrapModule} from '../../../angular-bootstrap-md';
 
