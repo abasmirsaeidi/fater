3263fd90d3e0999e4e18d4b6ee23b80a6c4fa4c2
Abbas Mirsaidi <usra1374@yahoo.com>
2018-07-26 15:15:27 +0430

    responsive home page & collection page

--- src/app/collections/collections.component.ts
+++ src/app/collections/collections.component.ts
@@ -64,33 +64,33 @@ export class CollectionsComponent implements OnInit {
     'other_program': [
       {
         'caption':
-          'به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود.',
+          'به متنی آزمایشی و بی‌ماحی گرافیک گفته می‌شود.',
         'img': 'https://anima-uploads.s3.amazonaws.com/5a47554c8aafab000d8aa728/5b02c4c93a51d5000d9d9c29/5b03c282fda4ae000d6d20ac/img/desktop-rectangle-13-copy 6.png',
       },
 
       {
         'caption':
-          'به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود.',
+          'به متنی آزمایشی و بی‌معنی در افیک گفته می‌شود.',
         'img': 'https://anima-uploads.s3.amazonaws.com/5a47554c8aafab000d8aa728/5b02c4c93a51d5000d9d9c29/5b03c282fda4ae000d6d20ac/img/desktop-rectangle-13-copy 1.png',
       },
       {
         'caption':
-          'به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود.',
+          'به متنی آزمایشی و بی‌معنایی و طراحی گرافیک گفته می‌شود.',
         'img': 'https://anima-uploads.s3.amazonaws.com/5a47554c8aafab000d8aa728/5b02b7c13a51d5000bd474f7/5b02b7c3c1baec000b6a06b0/img/desktop-rectangle-6%203.png',
       },
       {
         'caption':
-          'به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود.',
+          'به متنی آزمایشی و بی‌معراحی گرافیک گفته می‌شود.',
         'img': 'https://anima-uploads.s3.amazonaws.com/5a47554c8aafab000d8aa728/5b02b7c13a51d5000bd474f7/5b02b7c3c1baec000b6a06b0/img/desktop-rectangle-6%203.png',
       },
       {
         'caption':
-          'به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود.',
+          'به متنی آزمایشی و بی‌معطراحی گرافیک گفته می‌شود.',
         'img': 'https://anima-uploads.s3.amazonaws.com/5a47554c8aafab000d8aa728/5b02b7c13a51d5000bd474f7/5b02b7c3c1baec000b6a06b0/img/desktop-rectangle-6%203.png',
       },
       {
         'caption':
-          'به متنی آزمایشی و بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود.',
+          'به متنی آزمایشی و بی‌معحی گرافیک گفته می‌شود.',
         'img': 'https://anima-uploads.s3.amazonaws.com/5a47554c8aafab000d8aa728/5b02b7c13a51d5000bd474f7/5b02b7c3c1baec000b6a06b0/img/desktop-rectangle-6%203.png',
       },
     ],
--- src/app/news/news.module.ts
+++ src/app/news/news.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsComponent } from './news.component';
-import {RouterModule, Routes} from '@angular/router';
-import {MDBBootstrapModule} from '../../../angular-bootstrap-md';
+import { RouterModule, Routes } from '@angular/router';
+import { MDBBootstrapModule } from '../../../angular-bootstrap-md';
+import { WavesModule } from 'angular-bootstrap-md';
 
 const routes: Routes = [
   { path: '', component: NewsComponent}
@@ -11,6 +12,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
+    WavesModule,
     CommonModule,
     MDBBootstrapModule.forRoot(),
     RouterModule.forChild(routes),
